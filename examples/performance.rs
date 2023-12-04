a23f8cbe234661862b441bf0e8d6efd5efc8bbac
Tymofii Podolianko <tymopod-ipt25@lll.kpi.ua>
2023-12-04 19:53:15 +0200

    +pow

--- examples/performance.rs
+++ examples/performance.rs
@@ -3,38 +3,38 @@ use std::time;
 use std::time::Duration;
 use csv::Writer;
 use std::io;
+use vl_big_ints::UnsignedLongInt;
 use gf2::*;
 
 const OUTPUT_CSV: &str = "./report/data.csv";
-const EXPS: usize = 10000;
+const EXPS: usize = 1000;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Performance calculation example");
     let mut wrt = csv::Writer::from_path(OUTPUT_CSV)?;
 
 
-
-    for op in ["+","*","inverse","sqr"]{
+    for op in ["+", "*", "inverse", "sqr", "pow"] {
         let (op1, op2) = stage(&mut wrt);
         let avg_dur = measure(op, &op1, &op2, &mut wrt)?;
-        println!("Average for {}: {:?}",op, &avg_dur);
+        println!("Average for {}: {:?}", op, &avg_dur);
     }
 
     Ok(())
 }
 
-fn stage<T> ( writer: &mut Writer<impl io::Write>) -> (Vec<T>, Vec<T>)
-where T: From<[usize; 2*DIM]>
+fn stage<T>(writer: &mut Writer<impl io::Write>) -> (Vec<T>, Vec<T>)
+    where T: From<[usize; 2 * DIM]>
 {
     let mut op1: Vec<T> = Vec::with_capacity(EXPS);
     let mut op2: Vec<T> = Vec::with_capacity(EXPS);
 
     for _ in 0..EXPS {
-        let random_bytes: Vec<usize> = (0..2*DIM).map(|_| { rand::random::<usize>() }).collect();
+        let random_bytes: Vec<usize> = (0..2 * DIM).map(|_| { rand::random::<usize>() }).collect();
         let gf2 = T::from(random_bytes.try_into().unwrap());
         op1.push(gf2);
-        let random_bytes: Vec<usize> = (0..2*DIM).map(|_| { rand::random::<usize>() }).collect();
-        let gf2= T::from(random_bytes.try_into().unwrap());
+        let random_bytes: Vec<usize> = (0..2 * DIM).map(|_| { rand::random::<usize>() }).collect();
+        let gf2 = T::from(random_bytes.try_into().unwrap());
         op2.push(gf2);
     }
 
@@ -42,15 +42,22 @@ where T: From<[usize; 2*DIM]>
 }
 
 fn measure(op: &str, op1: &Vec<GF2Element<DIM>>, op2: &Vec<GF2Element<DIM>>, wrt: &mut csv::Writer<std::fs::File>) -> Result<Duration, Box<dyn Error>> {
-    println!("Running {} experiments", EXPS);
+    println!("Running {} experiments for each operation", EXPS);
 
     let mut durations: Vec<Duration> = Vec::with_capacity(EXPS);
     for i in 0..EXPS {
         let duration = match op {
             "+" => { measure_add(&op1[i], &op2[i]) }
-            "*" => { measure_mul(&op1[i], &op2[i]) },
-            "inverse" => { measure_inverse(&op1[i] )},
-            "sqr" => { measure_sqr(&op1[i] )},
+            "*" => { measure_mul(&op1[i], &op2[i]) }
+            "inverse" => { measure_inverse(&op1[i]) }
+            "sqr" => { measure_sqr(&op1[i]) }
+            "pow" => {
+                let mut op2: Vec<UnsignedLongInt> = Vec::with_capacity(EXPS);
+                let random_bytes: Vec<u64> = (0..DIM).map(|_| { rand::random::<u64>() }).collect();
+                op2.push(UnsignedLongInt::from(random_bytes.as_slice()));
+
+                measure_pow(&op1[i], &op2[i])
+            }
             _ => panic!("must be valid op string")
         };
 
@@ -68,7 +75,6 @@ fn measure(op: &str, op1: &Vec<GF2Element<DIM>>, op2: &Vec<GF2Element<DIM>>, wrt
 
     Ok(average_duration)
 }
-
 
 
 fn measure_add(op1: &GF2Element<DIM>, op2: &GF2Element<DIM>) -> time::Duration {
@@ -95,17 +101,17 @@ fn measure_sqr(op1: &GF2Element<DIM>) -> time::Duration {
     now.duration_since(then)
 }
 
-// fn measure_pow(op1: &GF2Element<DIM>, op2: &UnsignedLongInteger) -> time::Duration {
-//     let then = time::Instant::now();
-//     let _ = op1.mul(op2);
-//     let now = time::Instant::now();
-//
-//     now.duration_since(then)
-// }
-
 fn measure_inverse(op1: &GF2Element<DIM>) -> time::Duration {
     let then = time::Instant::now();
     let _ = op1.inverse();
+    let now = time::Instant::now();
+
+    now.duration_since(then)
+}
+
+fn measure_pow(op1: &GF2Element<DIM>, op2: &UnsignedLongInt) -> time::Duration {
+    let then = time::Instant::now();
+    let _ = op1.pow(op2);
     let now = time::Instant::now();
 
     now.duration_since(then)
